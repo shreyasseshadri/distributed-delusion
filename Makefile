ba42ac1bab1fa5e442a32fe4f070b2fc93fa6f84
shreyas <shreyasseshadri@gmail.com>
2020-06-14 13:06:45 +0530

    The right way

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ targets = linear-probing dht cached-lp
 all: $(targets)
 
 %: %.c
-	$(CC) -o $@ $@.c
+	$(CC) -o $@ $@.c test_cases.c
 
 clean:
 	rm -rf *.o $(targets)
--- test_cases.h
+++ test_cases.h
@@ -1,12 +1,1 @@
-int *test_keys;
-
-int test_keys_4_16[] = {2, 18, 34, 5, 21, 37, 53, 13, 29, 45, 61, 77};
-
-int* get_test_keys(int size,int N,int* test_size){
-	if(size == 4 && N == 16)
-	{
-		test_keys = test_keys_4_16;
-		*test_size =  sizeof(test_keys_4_16) / sizeof(test_keys_4_16[0]);
-	}
-	return test_keys;
-}
+int* get_test_keys(int size, int N, int *test_size);
