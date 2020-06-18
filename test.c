f631262e8471904f5007ba657dec01cc268adcfb
sanjayashokshetty <sanjayshetty2015@gmail.com>
2020-06-18 16:25:54 +0530

    time

--- test.c
+++ test.c
@@ -9,14 +9,15 @@
 void test_lp(struct LP_DHT *dht, int n, int *keys, int *values)
 {
 	clock_t t;
+	t = clock();
 	for (int i = 0; i < n; i++)
 	{
 		assert(lp_insert(dht, keys[i], values[i]) == CLP_SUCCESS);
 	}
 	int value;
-
+	t = clock() - t;
 	double time_taken = ((double)t) / CLOCKS_PER_SEC;
-    printf("ch insert time: %f ms\n", time_taken * 1000);
+    printf("LP insert time: %f ms\n", time_taken * 1000);
 
 	t = clock();
 	for (int i = 0; i < n; i++)
@@ -31,14 +32,15 @@ void test_lp(struct LP_DHT *dht, int n, int *keys, int *values)
 void test_clp(struct CLP_DHT *dht, int n, int *keys, int *values)
 {
 	clock_t t;
+	t = clock();
 	for (int i = 0; i < n; i++)
 	{
 		assert(clp_insert(dht, keys[i], values[i]) == CLP_SUCCESS);
 	}
 	int value;
-
+	t = clock() - t;
 	double time_taken = ((double)t) / CLOCKS_PER_SEC;
-    printf("ch insert time: %f ms\n", time_taken * 1000);
+    printf("CLP insert time: %f ms\n", time_taken * 1000);
 
 	t = clock();
 	for (int i = 0; i < n; i++)
